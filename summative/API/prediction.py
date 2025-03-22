420248dc864a341d98deb0915019529c8517ccad
Serwaa <c.akotonimo@alustudent.com>
2025-03-22 23:17:46 +0200

    app.get

--- summative/API/prediction.py
+++ summative/API/prediction.py
@@ -5,13 +5,19 @@ import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 import joblib 
 from datetime import datetime
+import os
 
-# Load the trained model
-model = joblib.load("summative/linear_regression/model.pkl")  
+
+
+class InputData(BaseModel):
+    Login_Date : datetime = Field(...,  description="the date of period start")
+    Cycle_Length: int = Field(..., ge=0, le=100, description="Value must be between 10 and 500")
+    Period_duration : int =  Field(..., ge=0, le=100, description="Value must be between 10 and 500")
+
+
 
 #initializing model 
 app = FastAPI()
-
 
 # Add CORS middleware to allow requests from any frontend
 app.add_middleware(
@@ -22,8 +28,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class InputData(BaseModel):
-    Login_Date : datetime = Field(...,  description="the date of period start")
-    Cycle_Length: int = Field(..., ge=0, le=100, description="Value must be between 10 and 500")
-    Period_duration : int =  Field(..., ge=0, le=100, description="Value must be between 10 and 500")
+# Load the trained model
+linear_model = joblib.load('summative/API/model.pkl')  
 
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the FastAPI server!"}
+
+
+
+@app.post("/predict")
+def predict(data: InputData):
+    login_timestamp = data.Login_Date.timestamp()  # Convert datetime to timestamp
+    input_features = [[login_timestamp, data.Cycle_Length, data.Period_Duration]]
+    prediction = linear_model.predict(input_features)
+    return {"prediction": prediction.tolist()}  # Convert NumPy array to a list for JSON response
+
+# Run the API using Uvicorn
+#if __name__ == "__main__":
+ #   port = int(os.environ.get("PORT", 8000))
+  #  uvicorn.run(app, host="0.0.0.0", port=port)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
